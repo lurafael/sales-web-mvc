4b30d4bf2d75dd78c0ef575f9372a620871f79aa
L. Ricci <lucasrafael.ricci@gmail.com>
2021-01-16 12:05:39 -0300

    First controller and Razor pages tests

--- SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales MVC App from C# Course";
+            ViewData["Author"] = "Lucas Rafael Ricci";
 
             return View();
         }
